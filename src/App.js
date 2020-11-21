fa4d310a4221a9c4ae7430322890e522363ca765
Greg Smelkov <gsmelkov@gmail.com>
2020-11-21 15:18:47 -0500

    Added proejcts section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Jumbotron, Row, Col, Container } from 'react-bootstrap';
-import { FaChevronCircleDown, FaGithub, FaLinkedin, FaHome, FaEnvelope, FaFilePdf, FaUserCircle } from 'react-icons/fa';
+import { Jumbotron, Row, Col, Container, Navbar, Nav } from 'react-bootstrap';
+import { FaChevronCircleDown, FaFileAlt, FaGithub, FaLinkedin, FaHome, FaEnvelope, FaFilePdf, FaUserCircle } from 'react-icons/fa';
 import Pdf from '../src/GregorySmelkovResume.pdf';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -26,7 +26,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <div style={{ height: "2000px" }}>
+      <div data-spy="scroll" data-target="#page-navbar" data-offset="0">
         <Jumbotron style={{ height: window.innerHeight }}>
           <div style={{ right: this.state.slideRight }} className="slide-title">
             <h1 className="slide-title-title">
@@ -39,6 +39,16 @@ class App extends React.Component {
           <div className="arrow"><a href="#about"><FaChevronCircleDown /></a></div>
         </Jumbotron>
 
+        <Navbar id="page-navbar">
+          <Nav>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#skills">Skills</Nav.Link>
+            <Nav.Link href="#education">Education</Nav.Link>
+            <Nav.Link href="#work">Work</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+          </Nav>
+        </Navbar>
+
         <section id="about">
           <Container>
             <Row>
@@ -50,7 +60,7 @@ class App extends React.Component {
                   <Row className="aboutme-blurb">
                     <Col>
                       <p>
-                        I'm a Computer Science student at UMass Lowell set to graduate in December 2020.
+                        I'm a passionate Computer Science student at UMass Lowell set to graduate in December 2020.
                       </p>
                     </Col>
                   </Row>
@@ -170,10 +180,12 @@ class App extends React.Component {
                     <Col xs={12} sm={6} className="place-name">
                       <h3>Text Machine Lab</h3>
                       <p>Jun 2018 - May 2019</p>
+                      <p><a className="place-link" target="_blank" rel="noopener noreferrer" href="https://arxiv.org/abs/1908.11443">NarrativeTime paper <FaFileAlt /></a></p>
                     </Col>
                     <Col xs={12} sm={6} className="place-about">
                       <h4>Research Intern</h4>
                       <p>Designed and developed an interface to annotate articles and other texts with regard to time for a temporal dataset (JavaScript, HTML)</p>
+                      <p>Co-author on NarrativeTime research paper about temporal annotation tool.</p>
                       <p>Worked to create an annotated call-to-action dataset to predict political unrest (Python)</p>
                       <p>Created a program to extract user conversations from Reddit for teaching chat bots (Python)</p>
                       <p>Wrote a script to scrape and format text from news articles for a temporal dataset (Python)</p>
@@ -189,6 +201,59 @@ class App extends React.Component {
                       <p>Taught game design, robotics, animation, and programming to elementary and middle school students</p>
                       <h4>Marketing/Operations Intern</h4>
                       <p>Gathered marketing information and generated promotional materials</p>
+                    </Col>
+                  </Row>
+                </article>
+              </Col>
+            </Row>
+          </Container>
+        </section>
+
+        <section id="projects">
+          <Container>
+            <Row>
+              <Col>
+                <article>
+                  <header>
+                    <h2>Projects</h2>
+                  </header>
+                  <Row className="place">
+                    <Col xs={12} sm={6} className="place-name">
+                      <h3>Cage Inventory Management System</h3>
+                      <p>June 2020 - Present</p>
+                      <p><a className="place-link" target="_blank" rel="noopener noreferrer" href="https://github.com/IsItGreg/CageIMS">View on GitHub <FaGithub /></a></p>
+                    </Col>
+                    <Col xs={12} sm={6} className="place-about">
+                      <h4>Camera inventory management web-app</h4>
+                      <p>Lead the design and development on a full-stack inventory management solution for the photography department at UMass Lowell.</p>
+                      <p>Built using React, Node.js, and MongoDB, this project was fun to create as it allowed me to learn those tools as well as RESTful APIs, managing user passwords, and all that goes into a larger project.</p>
+                      <p>The front-end UI was implemented in React using Bootstrap, Material-UI, Semantic UI, and Sass.</p>
+                    </Col>
+                  </Row>
+                  <Row className="place">
+                    <Col xs={12} sm={6} className="place-name">
+                      <h3>Personal Website</h3>
+                      <p>Aug 2020 - Present</p>
+                      <p><a className="place-link" target="_blank" rel="noopener noreferrer" href="https://github.com/IsItGreg/gsme.dev">View on GitHub <FaGithub /></a></p>
+                    </Col>
+                    <Col xs={12} sm={6} className="place-about">
+                      <h4>gsme.dev</h4>
+                      <p>This website was intended to be a digital version of my resume that allows for more detailed descriptions of what I have done.</p>
+                      <p>I built this site using React, Bootstrap, and hosted on GitHub pages and you can find the source code in the repository on the left.</p>
+                    </Col>
+                  </Row>
+                  <Row className="place">
+                    <Col xs={12} sm={6} className="place-name">
+                      <h3>Freshman / Sophomore Projects</h3>
+                      <p>Sep 2017 - Dec 2018</p>
+                    </Col>
+                    <Col xs={12} sm={6} className="place-about">
+                      <h4>Ants and Doodlebugs</h4>
+                      <p>Developed a small simulation to show a balance between predators and prey (C++)</p>
+                      <h4>Big Int</h4>
+                      <p>Created a class to handle mathematical operations with extremely large numbers by storing the data as strings (C++)</p>
+                      <h4>Evil Hangman</h4>
+                      <p>Programmed a console game of hangman where the computer will cheat to win (C)</p>
                     </Col>
                   </Row>
                 </article>
