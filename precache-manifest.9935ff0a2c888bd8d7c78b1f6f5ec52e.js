61344c2285cc00eb6bed646d72f1f6e134e3d4af
Greg Smelkov <gsmelkov@gmail.com>
2020-11-22 18:14:14 -0500

    Updates

---
+++ precache-manifest.9935ff0a2c888bd8d7c78b1f6f5ec52e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70a6a5535a7849c8be301fc3b0c92183",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f109a18a52122690314",
+    "url": "/static/css/2.437d98bc.chunk.css"
+  },
+  {
+    "revision": "5bd58e33d1fc7a4231b7",
+    "url": "/static/css/main.581ca36b.chunk.css"
+  },
+  {
+    "revision": "5f109a18a52122690314",
+    "url": "/static/js/2.63d92b3d.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/static/js/2.63d92b3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bd58e33d1fc7a4231b7",
+    "url": "/static/js/main.da0772e1.chunk.js"
+  },
+  {
+    "revision": "9f9ea5bdd9cf7a888773",
+    "url": "/static/js/runtime-main.80749240.js"
+  },
+  {
+    "revision": "553ee1307d180071b184bd58f5abaed3",
+    "url": "/static/media/GregorySmelkovResume.553ee130.pdf"
+  }
+]);
